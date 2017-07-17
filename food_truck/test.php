4691b3e22ebe1987f77389e714a30752653d7b54
Kyrrah Nork <kyrrahnork@gmail.com>
2017-07-17 15:43:00 -0700

    Delete test.php

--- food_truck/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-        include('menu_item_class.php');
-        include('taco_cart.php');
-        print '<pre>';
-        echo print_r($items);
-        print '</pre>';
-        echo '<br>';
-        echo print_r($_POST);
