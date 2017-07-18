43abb8f8e50cad61fcd2a186a314c18f6f6f6433
Kyrrah Nork <kyrrahnork@gmail.com>
2017-07-18 13:59:13 -0700

    Delete credentials.php

--- food_truck/includes/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-//credentials.php
-
-define('DB_NAME','central_lindesara_db');
-define('DB_USER','central_sara_db');
-define('DB_PASSWORD','Drearaigi1!');
-define('DB_HOST','mysql-1.lindesara.com');
