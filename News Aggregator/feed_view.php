6990d1fa3498160d2908e0b8599952d1922c4fc7
Ron Nims <rleenims@gmail.com>
2017-08-10 16:55:59 -0700

    reverts last commit that re- re- added the version with the Feed class

--- News Aggregator/feed_view.php
+++ News Aggregator/feed_view.php
@@ -14,42 +14,72 @@
  * @todo !!! implement persistent sessions and cached feed data
  * @todo NTH Add Feeds
  * @todo NTH Update Feeds
- */
+ 
 
 $time = $_SERVER[‘REQUEST_TIME’];
 
 /**
  * for a 30 minute timeout, specified in seconds
- */
+ 
 $timeout_duration = 600;
 
 /**
  * Here we look for the user’s LAST_ACTIVITY timestamp. If
  * it’s set and indicates our $timeout_duration has passed,
  * blow away any previous $_SESSION data and start a new one.
- */
+
 if (isset($_SESSION[‘LAST_ACTIVITY’]) && ($time-$_SESSION[‘LAST_ACTIVITY’]) > $timeout_duration) {
   session_unset();    
   session_destroy();
   session_start();    
 }
-   
+ */   
 /**
  * Finally, update LAST_ACTIVITY so that our timeout
  * is based on it and not the user’s login time.
- */
 $_SESSION[‘LAST_ACTIVITY’] = $time;
+ */
 
 require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
+require '../inc_0700/Pager_inc.php';
+require 'category.php';
+
+echo '<link href="' . VIRTUAL_PATH  . 'include/newsFeed.css" rel="stylesheet" type="text/css" />';
 
 # check variable of item passed in - if invalid data, forcibly redirect back to index.php page
-if(isset($_GET['id']) && (int)$_GET['id'] > 0){#proper data must be on querystring
-	 $feedID = (int)$_GET['id']; #Convert to integer, will equate to zero if fails
+if(isset($_REQUEST['id']) && (int)$_REQUEST['id'] > 0){#proper data must be on querystring
+	$feedID = (int)$_GET['id']; #Convert to integer, will equate to zero if fails
+
 }else{
 	myRedirect(VIRTUAL_PATH . "news/index.php");
 }
 
 $myFeed = new Feed($feedID);
+
+if(isset($_GET['act'])) {
+    $myAction = (trim($_GET['act']));
+
+    switch ($myAction) 
+    {//check 'act' for type of process
+        case "clear": // Cear feed cache
+            //TODO $myFeed->clearFeedCache() is not working
+            $myFeed->clearFeedCache();
+            //so clearing feed cach directly
+            //if ( isset($_SESSION['Feed' . $feedID]) ) {        
+            //    unset($_SESSION['Feed' . $feedID]);
+            //    unset($_SESSION['FeedTime' . $feedID]);
+            //}
+
+            myRedirect(THIS_PAGE . "?id=" . $feedID);
+    }
+}
+if(isset($_GET['catid'])) {
+	$categoryID = (int)$_GET['catid'];
+    $myCategory = new Category($categoryID);
+    $categoryName = $myCategory->Name;
+}else{
+    $categoryName = 'News';
+}
 
 if($myFeed->IsValid)
 {#only load data if record found
@@ -64,144 +94,39 @@ if($myFeed->IsValid)
     // $xml will contain the entire RSS Feed object for this feed
     $xml = $myFeed->getRSS();
 
-    echo '
-    <div class="fragment">
-    <h3 align="center"> Category: <i><b>' . $myFeed->CategoryName .
+    echo '<div class="fragment">
+    <h3 align="center">RSS News Feed Portal</h3>
+    <h3 align="center"> Category: <i><b>' . $categoryName .
         '</b></i>, RSS Feed: <i><b>' . $myFeed->Name . '</b></i></h3>
         <p align="center">' . $myFeed->Description . '</p>
-        <p align="center">' . $myFeed->timeStamp() . '</p>
-    </div>';
-    
-    
+        <p align="center">' . 'Feed loaded at: ' . $myFeed->timeStamp() . '
+        <a href="' . THIS_PAGE . '?id=' . $feedID . '&act=clear">Clear Cache</a></p></div>
+    ';
+    //<a href="' . THIS_PAGE . '?id=' . $feedID . '&act=clear">Clear Cache</a></p></div>
+   
     print '<h1 class="feedTitle">' . $xml->channel->title . '</h1>';
 
     foreach($xml->channel->item as $story) {
-        echo '<div class="fragment">' . $story->description . '</div><br />';
+        //echo '<a href="' . $story->link . '">' . $story->title . '</a><br />'; 
+        echo '<div class="fragment">
+        <p>' . $story->description . '</p>
+        <p>' . $story->pubDate . '</p>
+        </div><br /><br />';
     }
 }else{//no such Feed!
     echo '<div align="center">ERROR: No such Feed for ID=' . $feedID . '</div>';
 }
 
+
+//$prev = '<img src="' . VIRTUAL_PATH . 'arrow_prev.gif" border="0" />';
+//$next = '<img src="' . VIRTUAL_PATH . 'arrow_next.gif" border="0" />';
+
+# Create instance of new 'pager' class
+//$myPager = new Pager(5,'',$prev,$next,'');
+
 echo '<div align="center"><a href="' . VIRTUAL_PATH . 'news/index.php">Back</a></div>';
 
 get_footer(); #defaults to theme footer or footer_inc.php
 
-class Feed
-{
-    public $FeedID = 0;
-    public $NewsCategoryID = 0;
-    public $CategoryName = '';
-    public $Name= '';
-    public $Description= '';
-    public $Feed = '';
-    public $DateAdded = '';
-    public $LastUpdated = '';
-    public $IsValid = false;
-    
-    public function __construct($id)
-    {
-        $id = (int)$id;//cast to integer disallows SQL injection
 
-        $sql = "
-            SELECT
-                nf.FeedID,
-                nf.NewsCategoryID,
-                nc.Name 'CategoryName',
-                nf.Name,
-                nf.Description,
-                nf.Feed,
-                date_format(nf.DateAdded, '%W %D %M %Y %H:%i') 'DateAdded',
-                date_format(nf.LastUpdated, '%W %D %M %Y %H:%i') 'LastUpdated' 
-            FROM " . PREFIX . "news_feeds nf 
-            INNER JOIN " . PREFIX . "news_categories nc ON nf.NewsCategoryID = nc.NewsCategoryID 
-            WHERE nf.FeedID = " . $id;
-        
-        $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
-
-        if(mysqli_num_rows($result) > 0)
-        {#records exist - process
-            $this->IsValid = true;//record found!
-            while ($row = mysqli_fetch_assoc($result))
-            {
-                $this->FeedID = $id;
-                $this->NewsCategoryID = dbOut($row['NewsCategoryID']);
-                $this->CategoryName = dbOut($row['CategoryName']);
-                $this->Name = dbOut($row['Name']);
-                $this->Description = dbOut($row['Description']);
-                $this->Feed = dbOut($row['Feed']);
-                $this->DateAdded = dbOut($row['DateAdded']);
-                $this->LastUpdated = dbOut($row['LastUpdated']);
-            }
-        }
-
-        @mysqli_free_result($result); # We're done with the data!
-
-    }// END class Feed Constructor
-    
-    // function getRSS - return RSS Feed object from cache in Session var
-    public function getRSS()
-    {
-        if ( !isset($_SESSION['Feed' . $this->FeedID])) {
-        
-            //set a cookie
-            //setcookie("myCookie", $_SESSION['Feed' . $this->FeedID], time() + 600);
-            
-            // create unique session variable containing Feed object
-            $response = file_get_contents($this->Feed);
-            $_SESSION['Feed' . $this->FeedID] = $response;
-            // set/reset unique session variable
-            $_SESSION['FeedTime' . $this->FeedID] = getdate()['0'];
-        }else{
-            $response = $_SESSION['Feed' . $this->FeedID];  
-            //setcookie("myCookie", $_SESSION['Feed' . $this->FeedID], time() + 600);
-        }
-        
-        //if()
-        //{
-            
-        //}
-        
-        // return Feed object from the session variable
-        $rssObject = simplexml_load_string($response);
-        return $rssObject;
-    }
-    
-    // function timeStamp - return timestamp of latest cache data 
-    public function timeStamp()
-    {
-        $date = new DateTime();
-        $date->setTimestamp($_SESSION['FeedTime'.$this->FeedID]);
-        return $date->format('Y-m-d H:i:s') . "\n";
-    }
-/*    
-    
-    public function unsetCookie()
-    {
-        setcookie("myCookie",$_SESSION['Feed' . $this->FeedID],time()-1);
-    }
-*/    
-/*    
-    // function stale - has Cached Feed object expired?
-    private function stale()
-    {
-        define ('CACHE_TIMEOUT', 600); // in seconds, 600 = 10 minutes
-        
-        if ( isset($_SESSION['FeedTime' . $this->FeedID]) ) {        
-        
-            $expires = $_SESSION['FeedTime' . $this->FeedID] + CACHE_TIMEOUT;
-
-            if ( $expires < getdate()['0'] ) {
-                $expired = true;
-            }else{
-                $expired = false;
-            }
-        }else{
-            $expired = true;            
-        }
-        
-        return $expired;    
-    }
-*/    
-    
-} // END class Feed
 
